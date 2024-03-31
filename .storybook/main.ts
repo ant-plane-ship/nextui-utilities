8ba391cd26adc38744615b2988069adebb7c83f6
ant-plane-ship <ant-plane-ship@gmail.com>
2024-03-31 12:16:18 +0900

    WIP : publish storybook on Githug Pages

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,27 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import { withoutVitePlugins } from '@storybook/builder-vite';
 
 const config: StorybookConfig = {
-  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-onboarding",
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions"
-  ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
+    stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+    addons: [
+        "@storybook/addon-onboarding",
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@chromatic-com/storybook",
+        "@storybook/addon-interactions"
+    ],
+    framework: {
+        name: "@storybook/react-vite",
+        options: {},
+    },
+    docs: {
+        autodocs: "tag",
+    },
+    async viteFinal(config) {
+        return {
+            ...config,
+            plugins: await withoutVitePlugins(config.plugins, ['vite:dts'])
+        }
+    }
 };
 export default config;
